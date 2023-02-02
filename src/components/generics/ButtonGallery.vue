69314d713f680edb36668228f90a12b95af325da
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2023-02-02 12:59:30 +0100

    ButtonGallery generic component

---
+++ src/components/generics/ButtonGallery.vue
@@ -0,0 +1,12 @@
+<script>
+export default {
+    name: 'ButtonGallery',
+    props: { b_class: String, i_class: String }
+}
+</script>
+
+<template>
+    <button :class="b_class">
+        <i :class="i_class"></i>
+    </button>
+</template>
