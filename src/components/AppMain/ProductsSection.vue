ce7a5f7e19a1469ab83b924729c02c172b1bf1d7
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2023-02-02 13:00:20 +0100

    ButtonGallery component added in ProductsSection

--- src/components/AppMain/ProductsSection.vue
+++ src/components/AppMain/ProductsSection.vue
@@ -2,17 +2,22 @@
 import { data } from '../../assets/data';
 import ActionCall from '../generics/ActionCall.vue';
 import ProductsGallery from '../generics/ProductsGallery.vue';
+import ButtonGallery from '../generics/ButtonGallery.vue';
 export default {
     name: 'ProductsSection',
     data() { return { data, currIndex: 0 } },
-    components: { ActionCall, ProductsGallery },
+    components: { ActionCall, ProductsGallery, ButtonGallery },
 }
 </script>
 <template>
     <section id="products-gallery" class="container d-flex jc-between ai-center mv-section">
         <action-call :label="data.products.label" :title="data.products.title"
             :button_label="data.products.button"></action-call>
-        <products-gallery :gallery="data.gallery.slice(0, 2)"></products-gallery>
+        <div class="slider">
+            <products-gallery :gallery="data.gallery.slice(0, 2)"></products-gallery>
+            <button-gallery b_class="b prev" i_class="fa-solid fa-angle-left"></button-gallery>
+            <button-gallery b_class="b next" i_class="fa-solid fa-angle-right"></button-gallery>
+        </div>
     </section>
 </template>
 
